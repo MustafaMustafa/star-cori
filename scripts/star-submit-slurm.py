518e5b08a4609b061d15c46f85c49fb8837a6485
Mustafa Mustafa <mstftsm@gmail.com>
2016-04-30 10:26:29 -0700

    add STDOUT and STDERR. Add verbose and submit options

--- scripts/star-submit-slurm.py
+++ scripts/star-submit-slurm.py
@@ -4,7 +4,8 @@ __email__ = "mmustafa@lbl.gov"
 """ a script to create batch files for star data production using slurm """
 
 import os
-import sys
+import json
+import subprocess
 import argparse
 import binascii
 import math
@@ -18,18 +19,29 @@ CHAIN = 'DbV20150316 P2014a pxlHit istHit btof mtd mtdCalib BEmcChkStat CorrX OS
 
 OUT_DIR = '/project/projectdirs/starprod/rnc/mustafa/cori_test/prod'
 SBATCH_DIR = './sbatch'
-LOG_DIR = './log'
-ERR_DIR = './err'
+STDOUT_DIR = './log'
+STDERR_DIR = './err'
+DAQ_SPLIT_LOG = './daq_split_log'
+
+VERBOSE = False
+SUBMIT = False
 
 def get_args():
     parser = argparse.ArgumentParser(description="a script to create batch files for star data production using slurm")
-    parser.add_argument('--list', help='file containting list of files to process', action = 'store', type=str)
+    parser.add_argument('-l', '--list', help='file containting list of files to process', action = 'store', type=str)
+    parser.add_argument('-v', '--verbose', help='increase output verbosity', action = 'store_true', default = False)
+    parser.add_argument('-s', '--submit', help='submit jobs. Default is to create sbatch files only', action = 'store_true', default = False)
 
     args = parser.parse_args()
 
     if not args.list:
         print "Need list of files to process. Usage:\n"
         exit(1)
+
+    global VERBOSE
+    global SUBMIT
+    VERBOSE = args.verbose
+    SUBMIT = args.submit
 
     return args
 
@@ -73,20 +85,35 @@ def check_submission_env():
     
     errors = 0
 
+    submission_dir = os.getcwd()
     if not os.path.isdir(SBATCH_DIR):
         print "ERROR: SBATCH DIR = %s doesn't exist!"%SBATCH_DIR
         errors += 1
 
-    if not os.path.isdir(LOG_DIR):
-        print "ERROR: LOG DIR = %s doesn't exist!"%LOG_DIR
+    global STDOUT_DIR
+    if not os.path.isdir(STDOUT_DIR):
+        print "ERROR: LOG DIR = %s doesn't exist!"%STDOUT_DIR
         errors += 1
+    elif STDOUT_DIR[0] != '/':
+        if STDOUT_DIR[0] == '.':
+            STDOUT_DIR = STDOUT_DIR[2:]
+        STDOUT_DIR = submission_dir + '/' + STDOUT_DIR
 
-    if not os.path.isdir(ERR_DIR):
-        print "ERROR: ERR DIR = %s doesn't exist!"%ERR_DIR
+    global STDERR_DIR
+    if not os.path.isdir(STDERR_DIR):
+        print "ERROR: ERR DIR = %s doesn't exist!"%STDERR_DIR
         errors += 1
+    elif STDERR_DIR[0] != '/':
+        if STDERR_DIR[0] == '.':
+            STDERR_DIR = STDERR_DIR[2:]
+        STDERR_DIR = submission_dir + '/' + STDERR_DIR
 
     if not os.path.isdir(OUT_DIR):
-        print "ERROR: OUT DIR = %s doesn't exist!"%ERR_DIR
+        print "ERROR: OUT DIR = %s doesn't exist!"%OUT_DIR
+        errors += 1
+
+    if not os.path.isdir(DAQ_SPLIT_LOG):
+        print "ERROR: DAQ SPLIT DIR = %s doesn't exist!"%DAQ_SPLIT_LOG
         errors += 1
         
     return errors == 0
@@ -99,14 +126,19 @@ def make_sbatch_file(file, star_evt, end_evt, sub_idx):
     day = get_daynumber(runnumber)
     out_dir = '%s/%i/%i/%i'%(OUT_DIR,day,runnumber,sub_idx)
     scratch_dir = '%s_%i'%(SUBMISSION_ID,JOB_IDX)
+    log_dir = '%s/%i/%i'%(STDOUT_DIR,day,runnumber)
+    err_dir = '%s/%i/%i'%(STDERR_DIR,day,runnumber)
+    subprocess.call(['mkdir','-p',log_dir])
+    subprocess.call(['mkdir','-p',err_dir])
 
-    sbatch_file = open("%s/sched%s_%i.sbatch"%(SBATCH_DIR,SUBMISSION_ID,JOB_IDX),'w')
+    sbatch_filename = '%s/sched%s_%i.sbatch'%(SBATCH_DIR,SUBMISSION_ID,JOB_IDX)
+    sbatch_file = open(sbatch_filename,'w')
     sbatch_file.write('#!/bin/bash'+'\n')
     sbatch_file.write('#SBATCH --image=%s'%IMAGE+'\n')
     sbatch_file.write('#SBATCH --nodes=1'+'\n')
     sbatch_file.write('#SBATCH --partition=regular'+'\n')
-    sbatch_file.write('#SBATCH --output=%s/%s_%i.log'%(LOG_DIR,SUBMISSION_ID,JOB_IDX)+'\n')
-    sbatch_file.write('#SBATCH --error=%s/%s_%i.err'%(ERR_DIR,SUBMISSION_ID,JOB_IDX)+'\n')
+    sbatch_file.write('#SBATCH --output=%s/%s_%i.log'%(log_dir,SUBMISSION_ID,JOB_IDX)+'\n')
+    sbatch_file.write('#SBATCH --error=%s/%s_%i.err'%(err_dir,SUBMISSION_ID,JOB_IDX)+'\n')
     sbatch_file.write('#SBATCH --time=%s'%get_time(star_evt,end_evt)+'\n')
     sbatch_file.write('\n')
     sbatch_file.write('module load shifter\n')
@@ -120,6 +152,8 @@ def make_sbatch_file(file, star_evt, end_evt, sub_idx):
     sbatch_file.write('cp -p %s.*.root %s\n'%(base_name,out_dir))
     sbatch_file.write('rm -r -f %s\n'%scratch_dir)
     sbatch_file.close()
+
+    return sbatch_filename
 
 def process_file(file):
     total_time = file[1] * CPU_PER_EVENT
