9940ffa7456ef89559d3007611ba7d30d2a8ec0b
Mustafa Mustafa <mstftsm@gmail.com>
2016-07-19 12:26:09 -0700

    Initial commit

---
+++ production-pipeline/daqFilesWatcher.py
@@ -0,0 +1,68 @@
+#!/bin/usr/python
+__author__ = "Mustafa Mustafa"
+__email__ = "mmustafa@lbl.gov"
+
+"""
+Daemon to watch for daq files at a specific path and populate the DB
+"""
+
+import logging
+import argparse
+import sys
+import yaml
+import os
+from mongoDbUtil import mongoDbUtil
+
+# global variables
+__verbose = False
+__daqFilesPath = ''
+
+def main():
+    logFormat = '%(asctime)-15s %(levelname)s: %(message)s'
+    logging.basicConfig(level=logging.INFO, format=logFormat)
+
+    args = get_args()
+    load_configuration(args.configuration)
+
+    db = mongoDbUtil('admin').db
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Daemon to watch for daq files at a specific path and populate the DB")
+    required = parser.add_argument_group('required arguments')
+    required.add_argument('-c','--configuration', help='configuration file', action = 'store', type=str)
+    parser.add_argument('-v', '--verbose', help='increase output verbosity', action = 'store_true', default = False)
+
+    args = parser.parse_args()
+
+    if not args.configuration:
+        logging.error("Need configuration file")
+        logging.info("Usage: %s -c configuration.yaml"%sys.argv[0])
+        exit(1)
+
+    global __verbose
+    __verbose = args.verbose
+
+    return args
+
+def load_configuration(configuration_file):
+
+    if os.path.exists(configuration_file):
+        logging.info("Loading configuration file ...")
+    else:
+        logging.error("Configuration file %s doesn't exist!"%configuration_file)
+        exit(1)
+
+    f = file(configuration_file,'r')
+    parameters = yaml.load(f)
+    f.close()
+
+    __daqFilesPath = parameters['daqFilesPath']
+
+    if os.path.isdir(__daqFilesPath):
+        logging.info("Set to watch %s"%__daqFilesPath)
+    else:
+        logging.error("Path %s does not exist or is not a directory!"%__daqFilesPath)
+        exit(1)
+
+if __name__ == '__main__':
+    main()
