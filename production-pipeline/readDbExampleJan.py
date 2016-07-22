66773a3b4de6a66b7374d09cf6d95efdbe5fa889
Jan Balewski <balewski@lbl.gov>
2016-07-22 11:14:49 -0700

    ok

---
+++ production-pipeline/readDbExampleJan.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+""" Example use of mongoDB swiss knife """
+
+import datetime
+    
+from MongoDbUtil import MongoDbUtil
+
+def main():
+
+    dbColl = MongoDbUtil('ro').database()['daqFilesWatcher']
+    print dbColl
+
+    mxN=dbColl.count()
+    k=0
+    nShow=4
+    print 'show  last ',nShow, ' records of ',mxN
+    for it in dbColl.find().skip(mxN-nShow):
+        if k==0:
+            print it
+        k+=1    
+        print k, it['_id'], it['date'], it['totalNumberOfFilesSeen']
+
+
+
+    from bson.objectid import ObjectId
+    mins = 20
+    gen_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=mins) 
+    dummy_id = ObjectId.from_datetime(gen_time)
+    result = list(dbColl.find({"_id": {"$gte": dummy_id}}))
+
+
+    print 'seen records=', len(result),  'in last ',datetime.timedelta(minutes=mins), 'minutes'
+
+    nr=len(result)
+    delT=result[nr-1]['date'] - result[0]['date']
+    delFile=result[nr-1]['totalNumberOfFilesSeen'] - result[0]['totalNumberOfFilesSeen']
+    print 'two records diff:  delT=',delT, ' delFile=',delFile
+    
+    unixNow=datetime.datetime.utcnow();
+    unixLastRec=result[nr-1]['date']
+    unixDiff=unixNow-unixLastRec
+    difSec=unixDiff.total_seconds()
+    print 'last record age/sec',difSec, ' hh:mm:ss=',unixDiff,' now(utc)=',unixNow,' lastRec=',unixLastRec
+
+
+
+
+if __name__ == '__main__':
+    main()
