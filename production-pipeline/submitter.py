6d0bcb92325cddbe5a62dff67f0f750c9708ce26
Mustafa Mustafa <mstftsm@gmail.com>
2016-07-27 08:29:26 -0700

    First commit

---
+++ production-pipeline/submitter.py
@@ -0,0 +1,40 @@
+#!/bin/usr/python
+"""Daemon to re/submit jobs according to files state in DB"""
+
+import sys
+import time
+import logging
+from MongoDbUtil import MongoDbUtil
+from StatsHeartbeat import StatsHeartbeat
+from load_configuration import get_args
+from load_configuration import load_configuration
+
+__author__ = "Mustafa Mustafa"
+__email__ = "mmustafa@lbl.gov"
+
+# pylint: disable=C0103
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)-15s - %(levelname)s - %(module)s : %(message)s')
+__logger = logging.getLogger(__name__)
+# pylint: enable=C0103
+
+def main():
+    """re/submit jobs according to files state in DB"""
+
+    args = get_args(__doc__)
+    config = load_configuration(args.configuration)
+
+    database = MongoDbUtil('admin', db_server=config['db_server'], db_name=config['db_name']).database()
+
+    # spawn a stats heartbeat
+    accum_stats = {'submitted': 0}
+    stats = {'pending': 0}
+
+    stats_heartbeat = StatsHeartbeat(config['heartbeat_interval'],
+                                     database[config['db_collection']],
+                                     accum_stats, stats)
+    logging.info("Heartbeat daemon spawned")
+
+    while True:
+        time.sleep(config['submit_sleep_interval'])
+if __name__ == '__main__':
+    main()
