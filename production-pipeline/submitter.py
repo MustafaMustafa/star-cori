7cdb08c700366cde9724bf6bb98301746397002b
Mustafa Mustafa <mstftsm@gmail.com>
2016-07-29 12:52:09 -0700

    Implement worker

--- production-pipeline/submitter.py
+++ production-pipeline/submitter.py
@@ -5,8 +5,10 @@ import sys
 import time
 import logging
 import load_configuration
+import pymongo
 from MongoDbUtil import MongoDbUtil
 from StatsHeartbeat import StatsHeartbeat
+from StarSubmitSlurmEngine import StarSubmitSlurmEngine
 
 __author__ = "Mustafa Mustafa"
 __email__ = "mmustafa@lbl.gov"
@@ -27,21 +29,68 @@ def submitter(config_file):
     """re/submit jobs according to files state in DB"""
 
     config = load_configuration.load_configuration(config_file)
-    config = load_configuration.affix_production_tag(config, ['db_collection', 'db_production_files_collection'])
+    config = load_configuration.affix_production_tag(config, ['db_collection',
+                                                              'db_production_files_collection',
+                                                              'db_jobs_validator_collection'])
 
     database = MongoDbUtil('admin', db_server=config['db_server'], db_name=config['db_name']).database()
 
     # spawn a stats heartbeat
-    accum_stats = {'submitted': 0}
-    stats = {'pending': 0}
+    accum_stats = {'ever_submitted': 0}
+    stats = {'pending': 0, 'submitted': 0, 'resubmitted':0}
 
     stats_heartbeat = StatsHeartbeat(config['heartbeat_interval'],
                                      database[config['db_collection']],
                                      accum_stats, stats)
     logging.info("Heartbeat daemon spawned")
 
+    # main work - loop over production files collection and submit jobs
+    star_submit_engine = StarSubmitSlurmEngine(config)
+    files_coll = database[config['db_production_files_collection']]
+    jobs_coll = database['db_jobs_validator_collection']
+
     while True:
+
+        stats = {'pending': 0, 'submitted': 0, 'resubmitted':0}
+
+        empty_job_slots = config['max_jobs_in_queue'] - number_of_jobs_in_queue(jobs_coll)
+
+        for daq in files_coll.find({'$or': [{'submitted': 0}, {'$where': 'this.failed == this.submitted'}]}):
+
+            if daq['failed'] == config['max_resubmissions']:
+                continue
+
+            if empty_job_slots:
+                updated_daq = star_submit_engine.process_job(daq)
+                updated_daq['submitted'] += 1
+                updated_daq['status'] = 'queued'
+
+                stats['submitted'] += 1
+                accum_stats['ever_submitted'] += 1
+
+                if daq['failed']:
+                    updated_daq['failed'] += 1
+                    stats['resubmitted'] += 1
+
+                files_coll.update_one({'_id':daq['_id']}, {'$set': updated_daq}, upsert=False)
+                empty_job_slots -= 1
+            else:
+                stats['pending'] += 1
+
+        stats_heartbeat.accum_stats = accum_stats
+        stats_heartbeat.stats = stats
+
         time.sleep(config['submit_sleep_interval'])
+
+def number_of_jobs_in_queue(coll):
+    """return number of queued jobs"""
+
+    if coll.count():
+        last_doc = coll.find().skip(coll.count()-1)[0]
+        return last_doc['stats']['total_in_queue']
+    else:
+        return 0
+
 
 if __name__ == '__main__':
     main()
