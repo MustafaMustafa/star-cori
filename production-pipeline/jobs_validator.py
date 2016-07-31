1d796e471abbdde978b35f1405002876e4ed1a0d
Mustafa Mustafa <mstftsm@gmail.com>
2016-07-31 13:05:20 -0700

    Check if output MuDst pass the QA

--- production-pipeline/jobs_validator.py
+++ production-pipeline/jobs_validator.py
@@ -1,9 +1,11 @@
 #!/bin/usr/python
 """Daemon to watch jobs in slurm queue and validate output of completed jobs"""
 
+import os
 import sys
 import time
 import logging
+import yaml
 import load_configuration
 import slurm_utility
 from MongoDbUtil import MongoDbUtil
@@ -33,7 +35,7 @@ def jobs_validator(config_file):
     database = MongoDbUtil('admin', db_server=config['db_server'], db_name=config['db_name']).database()
 
     # spawn a stats heartbeat
-    accum_stats = {'completed': 0, 'failed': 0}
+    accum_stats = {'completed_job': 0, 'completed_muDst': 0, 'failed_job':0, 'failed_muDst': 0}
     stats = {'total_in_queue': 0, 'running': 0, 'pending': 0, 'failed': 0, 'completing': 0, 'unknown': 0}
 
     stats_heartbeat = StatsHeartbeat(config['heartbeat_interval'],
@@ -74,6 +76,14 @@ def jobs_validator(config_file):
                 state = job_stats['state']
                 if state == 'COMPLETED':
                     job['state'] = 'COMPLETED'
+                    accum_stats['completed_job'] += 1
+
+                    if not pass_qa(job):
+                        job['failed'] += 1
+                        accum_stats['failed_muDst'] += 1
+                    else:
+                        accum_stats['completed_muDst'] += 1
+
                     job['Elapsed'] = job_stats['Elapsed']
                     job['CPUTime'] = job_stats['CPUTime']
                     job['CpuEff'] = job_stats['CpuEff']
@@ -81,12 +91,11 @@ def jobs_validator(config_file):
                     job['MaxVMSize'] = job_stats['MaxVMSize']
                     job['Reserved'] = job_stats['Reserved']
                     files_coll.update_one({'_id':job['_id']}, {'$set': job}, upsert=False)
-                    accum_stats['completed'] += 1
                 elif state == 'FAILED':
-                    job['failed'] = 'FAILED'
-                    files_coll.update_one({'_id':job['_id']}, {'$set': job}, upsert=False)
-                    accum_stats['failed'] += 1
+                    accum_stats['failed_job'] += 1
                     stats['failed'] += 1
+                    job['failed'] += 1
+                    files_coll.update_one({'_id':job['_id']}, {'$set': job}, upsert=False)
                 else:
                     stats['unknown'] += 1
 
@@ -94,6 +103,13 @@ def jobs_validator(config_file):
         stats_heartbeat.stats = stats
 
         time.sleep(config['recheck_sleep_interval'])
+
+def pass_qa(job):
+    """Returns status from checkProduction log"""
+
+    with open(os.path.join(os.path.dirname(job['log']), '%s.nEventsCheck.yaml'%job['basename']), 'r') as ftmp:
+        results = yaml.load(ftmp)
+        return results['Status'] == 'good'
 
 if __name__ == '__main__':
     main()
