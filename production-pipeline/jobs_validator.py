93e34822a731ff2dbcef88788e0c52fd1ee049a4
Mustafa Mustafa <mstftsm@gmail.com>
2016-07-30 19:31:48 -0700

    Update db on jobs status from slurm

--- production-pipeline/jobs_validator.py
+++ production-pipeline/jobs_validator.py
@@ -34,7 +34,7 @@ def jobs_validator(config_file):
 
     # spawn a stats heartbeat
     accum_stats = {'completed': 0, 'failed': 0}
-    stats = {'total_in_queue': 0, 'running': 0, 'pending': 0, 'failed': 0}
+    stats = {'total_in_queue': 0, 'running': 0, 'pending': 0, 'failed': 0, 'completing': 0, 'unknown': 0}
 
     stats_heartbeat = StatsHeartbeat(config['heartbeat_interval'],
                                      database[config['db_collection']],
@@ -45,8 +45,53 @@ def jobs_validator(config_file):
     files_coll = database[config['db_production_files_collection']]
 
     while True:
-        for job in files_coll.find({'$or': [{'status': 'queued'}, {'status': 'running'}]}):
-            pass
+
+        slurm_jobs = slurm_utility.get_queued_jobs(config['slurm_user'])
+        stats = {'total_in_queue': len(slurm_jobs), 'running': 0, 'pending': 0, 'failed': 0, 'completing': 0, 'unknown': 0}
+
+        for job in files_coll.find({'$or': [{'status': 'PENDING'}, {'status': 'RUNNING'}]}):
+
+            #job is still in queue, update info
+            if job['slurm_id'] in slurm_jobs:
+
+                state = slurm_jobs[job['slurm_id']]
+                if state == 'PENDING':
+                    stats['pending'] += 1
+                elif state == 'RUNNING':
+                    stats['running'] += 1
+                    if state != job['state']:
+                        job['state'] = 'RUNNING'
+                        files_coll.update_one({'_id':job['_id']}, {'$set': job}, upsert=False)
+                elif state == 'COMPLETING':
+                    stats['completing'] += 1
+                else:
+                    stats['unknown'] += 1
+
+            #job is out of queue, check status
+            else:
+
+                job_stats = slurm_utility.get_job_stats(job['slurm_id'])
+                state = job_stats['state']
+                if state == 'COMPLETED':
+                    job['state'] = 'COMPLETED'
+                    job['Elapsed'] = job_stats['Elapsed']
+                    job['CPUTime'] = job_stats['CPUTime']
+                    job['CpuEff'] = job_stats['CpuEff']
+                    job['MaxRSS'] = job_stats['MaxRSS']
+                    job['MaxVMSize'] = job_stats['MaxVMSize']
+                    files_coll.update_one({'_id':job['_id']}, {'$set': job}, upsert=False)
+                    accum_stats['completed'] += 1
+                elif state == 'FAILED':
+                    job['failed'] = 'FAILED'
+                    files_coll.update_one({'_id':job['_id']}, {'$set': job}, upsert=False)
+                    accum_stats['failed'] += 1
+                    stats['failed'] += 1
+                else:
+                    stats['unknown'] += 1
+
+        stats_heartbeat.accum_stats = accum_stats
+        stats_heartbeat.stats = stats
+
         time.sleep(config['recheck_sleep_interval'])
 
 if __name__ == '__main__':
