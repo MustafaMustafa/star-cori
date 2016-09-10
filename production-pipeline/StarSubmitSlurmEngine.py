600a3a368ea4ad8c8615bb3c6060f8e50837f92a
Mustafa Mustafa <mstftsm@gmail.com>
2016-09-10 12:19:33 -0700

    Use perCacheNode and local MySQL server

--- production-pipeline/StarSubmitSlurmEngine.py
+++ production-pipeline/StarSubmitSlurmEngine.py
@@ -25,6 +25,7 @@ class StarSubmitSlurmEngine(object):
 
         self.__submit = parameters['submit']
         self.__shifter_img = parameters['shifter_img']
+        self.__stardb_payload = parameters['stardb_payload']
         self.__production_chain = parameters['production_chain']
         self.__queue = parameters['queue']
         self.__cputime_per_event = parameters['cputime_per_event']
@@ -34,7 +35,6 @@ class StarSubmitSlurmEngine(object):
         self.__sbatch_dir = os.path.abspath(parameters['sbatch_dir'])
         self.__mpi_binary = os.path.abspath(parameters['mpi_binary'])
         self.__production_file_extensions = parameters['extensions']
-        self.__clean_scratch = parameters['clean_scratch']
         self.__submission_id = binascii.hexlify(os.urandom(16)).decode('utf-8')
         self.__job_index = -1
 
@@ -106,27 +106,39 @@ class StarSubmitSlurmEngine(object):
     def __make_sbatch_file(self, job_parameters):
         """Create production/sbatch/log/err directories and make sbatch file"""
 
+        scratch_dir = os.getenv('SCRATCH', 'FALSE')
+        stardb_dir_name = os.path.basename(os.path.normpath(self.__stardb_payload))
+
+        if scratch_dir == 'FALSE':
+            logging.error('$SCRATCH is not defined. Cannot proceed!')
+            exit(1)
+
         job_scratch = job_parameters['submission_idx']
         sbatch_file = open(job_parameters['sbatch'], 'w')
         sbatch_file.write('#!/bin/bash'+'\n')
         sbatch_file.write('#SBATCH --image=%s'%self.__shifter_img+'\n')
+        sbatch_file.write('#SBATCH --volume=%s/%s:/mnt:perNodeCache=size=100G\n'%(scratch_dir, job_parameters['submission_idx']))
         sbatch_file.write('#SBATCH --ntasks=%i'%job_parameters['number_of_cores']+'\n')
         sbatch_file.write('#SBATCH --partition=%s'%job_parameters['queue']+'\n')
         sbatch_file.write('#SBATCH --output=%s'%job_parameters['log']+'\n')
         sbatch_file.write('#SBATCH --error=%s'%job_parameters['err']+'\n')
         sbatch_file.write('#SBATCH --time=%s'%job_parameters['estimated_running_time']+'\n')
         sbatch_file.write('\n')
-        sbatch_file.write('#Prepare environment...\n')
-        sbatch_file.write('cd $SCRATCH\n')
-        sbatch_file.write('mkdir %s\n'%job_scratch)
-        sbatch_file.write('cd  %s\n'%job_scratch)
-        sbatch_file.write('cp %s .'%self.__mpi_binary+'\n')
-        sbatch_file.write('\n')
         sbatch_file.write('module load shifter\n')
         sbatch_file.write('shifter /bin/csh <<EOF\n')
         sbatch_file.write('source /usr/local/star/group/templates/cshrc\n')
         sbatch_file.write('source /usr/local/star/group/templates/enable_scl\n')
         sbatch_file.write('\n')
+        sbatch_file.write('#Prepare DB...\n')
+        sbatch_file.write('cd /mnt\n')
+        sbatch_file.write('echo "DB cp time is : "\n')
+        sbatch_file.write('time cp -r -p %s .\n'%self.__stardb_payload)
+        sbatch_file.write('/usr/bin/mysqld_safe --defaults-file=/mnt/%s/my.cnf --skip-grant-tables &\n'%stardb_dir_name)
+        sbatch_file.write('setenv DB_SERVER_LOCAL_CONFIG "/mnt/%s/dbLoadBalancerLocalConfig_generic.xml"\n'%stardb_dir_name)
+        sbatch_file.write('sleep 30\n')
+        sbatch_file.write('\n')
+        sbatch_file.write('#Run job...\n')
+        sbatch_file.write('cp %s .'%self.__mpi_binary+'\n')
         sbatch_file.write('%s\n'%job_parameters['command'])
         sbatch_file.write('\n')
 
@@ -142,22 +154,17 @@ class StarSubmitSlurmEngine(object):
             sbatch_file.write('root4star -l -b -q -x \'checkProduction.C(\"%s.MuDst.root\", %i)\''%(job_parameters['basename'], job_parameters['number_of_events']))
             sbatch_file.write('\nwait\n')
             sbatch_file.write('\n')
-        sbatch_file.write('EOF\n')
-        sbatch_file.write('\n')
 
         sbatch_file.write('#Copy back output files...\n')
         for ext in self.__production_file_extensions:
             if ext == 'MuDst.root':
                 sbatch_file.write('cp -p %s.nEventsCheck.yaml %s\n'%(job_parameters['basename'], os.path.dirname(job_parameters['log'])))
                 sbatch_file.write('cp -p %s.%s %s\n'%(job_parameters['basename'], ext, job_parameters['production_dir']))
+                sbatch_file.write('wait\n')
             else:
                 sbatch_file.write('cp -p *.%s %s\n'%(ext, job_parameters['production_dir']))
-        sbatch_file.write('\n')
+                sbatch_file.write('wait\n')
 
-        if self.__clean_scratch:
-            sbatch_file.write('#Clean up...\n')
-            sbatch_file.write('cd ../\n')
-            sbatch_file.write('rm -r -f %s\n'%job_scratch)
-
+        sbatch_file.write('EOF')
         sbatch_file.close()
 # pylint: enable=too-many-instance-attributes
