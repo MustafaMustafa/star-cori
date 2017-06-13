a9b245a43374d08aab4234313d0ac1bda3c6b25a
Mustafa Mustafa <mstftsm@gmail.com>
2017-06-13 15:28:15 -0700

    Implement buffers cleaner

--- production-pipeline/buffers_cleaner.py
+++ production-pipeline/buffers_cleaner.py
@@ -2,8 +2,10 @@
 """Daemon to clean pipline input and ouput buffers"""
 
 import sys
+import os
 import time
 import logging
+import tarfile
 import load_configuration
 from MongoDbUtil import MongoDbUtil
 from StatsHeartbeat import StatsHeartbeat
@@ -32,16 +34,56 @@ def buffers_cleaner(config_file):
     database = MongoDbUtil('admin', db_server=config['db_server'], db_name=config['db_name']).database()
 
     # spawn a stats heartbeat
-    accum_stats = {'cleaned_daqs': 0, 'cleaned_dsts': 0}
-    stats = {'dsts_on_disk': 0}
-
     stats_heartbeat = StatsHeartbeat(config['heartbeat_interval'],
                                      database[config['db_collection']],
-                                     accum_stats, stats)
+                                     accum_stats={'cleaned_prod': 0},
+                                     stats={'missing_prod': 0, 'tar_failed': 0})
     logging.info("Heartbeat daemon spawned")
 
+    files_coll = database[config['db_production_files_collection']]
     while True:
+        stats = {'dsts_on_disk': 0, 'missing_prod': 0, 'tar_failed': 0}
+
+        for job in files_coll.find({'status': 'COMPLETED'}):
+
+            # check all output files
+            if not check_prod_files(job):
+                stats['missing_prod'] += 1
+                continue
+
+            tFile = tarfile.open('%s/%s.cori.tar'%(config['tar_dir'], job['basename']), 'w')
+
+            for prod_file in job['production_files']:
+                tFile.add(prod_file)
+
+            tFile.add(job['log'])
+            tFile.add(job['err'])
+            tFile.add(os.path.join(os.path.dirname(job['log']), '%s.nEventsCheck.yaml'%job['basename']))
+
+            try:
+                tFile.close()
+            except:
+                stats['tar_failed'] += 1
+                continue
+
+            os.remove(job['daq_path'])
+            for prod_file in job['production_files']:
+                os.remove(prod_file)
+            stats_heartbeat.accum_stats['cleaned_prod'] += 1
+
+            job['status'] = 'TARBALLED'
+            files_coll.update_one({'_id':job['_id']}, {'$set': job}, upsert=False)
+
+        stats_heartbeat.stats = stats
         time.sleep(config['recheck_sleep_interval'])
+
+def check_prod_files(job):
+
+    for prod_file in job['production_files']:
+        if not os.path.exists(prod_file):
+            return False
+
+    return True
 
 if __name__ == '__main__':
     main()
